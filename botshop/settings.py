2720f3ac078a8d0c706b245421c3ced451b28dcf
Alexey Bavykin <alexey.bavykin@gmail.com>
2017-04-02 05:35:23 +1000

    always redirect http -> https

--- botshop/settings.py
+++ botshop/settings.py
@@ -145,6 +145,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #MICROBOT_WEBHOOK_DOMAIN = env('MICROBOT_WEBHOOK_DOMAIN', default=None)
 MICROBOT_WEBHOOK_DOMAIN = 'tranquil-lake-76341.herokuapp.com'
 TELEGRAM_BOT_HANDLERS_CONF = "bot.handlers"
+SECURE_SSL_REDIRECT = True
+
 
 LOGGING = {
     'version': 1,
