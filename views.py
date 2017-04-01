1330fa90cbe618be9e3a4548a30bf01264f48d3d
Alexey Bavykin <alexey.bavykin@gmail.com>
2017-04-01 19:08:22 +1100

    Fix

--- views.py
+++ views.py
@@ -4,6 +4,10 @@ import logging
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.urlresolvers import reverse
+from telegrambot.models import Bot, AuthToken
+from telegram import Bot as BotAPI
+
+
 
 def index(request):
     r = requests.get('http://httpbin.org/status/418')
@@ -13,5 +17,15 @@ def index(request):
     logger = logging.getLogger('my_app_name.my_new_module')
     logger.info(webhook)
     logger.info("!!!!!!!!!!!!!!")
+
+    from django.contrib.sites.models import Site
+    current_site = Site.objects.get_current()
+    url = 'https://' + current_site.domain + webhook
+
+    bot = BotAPI('339030622:AAGk24GWW40hJBfqOxqvhdsUJMw94zu5O98')
+    #bot.handle(Update.de_json(request.data, bot))
+    #bot.setWebhook(webhook_url=url)
+    logger.info(url)
+
 
     return HttpResponse('<pre>' + webhook + '</pre>')
