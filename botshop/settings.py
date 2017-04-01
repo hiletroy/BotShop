f8689ff98ead7d679a15407e29bc6ba954be8ea5
Alexey Bavykin <alexey.bavykin@gmail.com>
2017-04-02 00:15:04 +1100

    fixup! Fix

--- botshop/settings.py
+++ botshop/settings.py
@@ -112,7 +112,7 @@ SITE_ID = 1
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Australia/Melbourne'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
