2c2f1af6c5b8430b035f720d72af0a8d112e3af7
Alexey Bavykin <alexey.bavykin@gmail.com>
2017-04-01 23:53:03 +1100

    Fix

--- botshop/views.py
+++ botshop/views.py
@@ -1,10 +1,10 @@
 from telegrambot.bot_views.generic import TemplateCommandView
 
 class StartCommandView(TemplateCommandView):
-    template_text = "bot/messages/command_start_text.txt"
+    template_text = "messages/command_start_text.txt"
 
 class HelpCommandView(TemplateCommandView):
-    template_text = "bot/messages/command_help_text.txt"
+    template_text = "messages/command_help_text.txt"
 
 class UnknownCommandView(TemplateCommandView):
-    template_text = "bot/messages/command_unknown_text.txt"
+    template_text = "messages/command_unknown_text.txt"
